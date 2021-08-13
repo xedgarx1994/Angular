0e7c47404ca8fa7909e9396457c6f9c8fc22a7ce
Edgar <ramos.edgar.1994@gmail.com>
2021-08-12 20:58:33 -0500

    .

--- front-end/src/app/seguridad/seguridad.service.ts
+++ front-end/src/app/seguridad/seguridad.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { credencialesUsuario, respuestaAutenticacion } from './seguridad';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
